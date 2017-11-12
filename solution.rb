bba5b91323f95dbc68fa309a5a261bad7cb59872
nicolasrf91 <nicolas.rf91@gmail.com>
2017-11-12 07:54:28 -0500

    solution2_sinatra

--- solution.rb
+++ solution.rb
@@ -2,15 +2,26 @@ require 'sinatra'
 
 
 
-get '/' do
+#get '/' do
 
-		@nombre = params[:nombre]
+#		@nombre = params[:nombre]
 
-		if @nombre 
+#		if @nombre 
 
-			"<h1> Hola #{@nombre} !</h1>"
-		else	
-			"Hola desconocido!"
-	end
+#			"<h1> Hola #{@nombre} !</h1>"
+#		else	
+#			"Hola desconocido!"
+#	end
 
+#end
+
+
+
+get '/makers/:nombre' do
+ @nombre = params[:nombre]
+   if @nombre
+     "<h1>Hola #{@nombre.capitalize}!</h1>"
+   else
+     "Hola desconocido!"
+   end
 end
